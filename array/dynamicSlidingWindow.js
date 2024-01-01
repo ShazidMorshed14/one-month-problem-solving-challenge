4b3e5bcf2e5684dbeccd8fe50d814ab0b78d3b89
ShazidMorshed14 <shazidmorshed14@gmail.com>
2024-01-01 23:59:42 +0600

    dynamic sliding window problem explained

---
+++ array/dynamicSlidingWindow.js
@@ -0,0 +1,15 @@
+/**************************
+ * Find the shortest subarray that is equal or greater than x=7
+ * i/p---->[1,2,3,4,5,6]
+ * o/p----->2 (length)
+ *
+ */
+
+const shortestSubArray = (arr, x) => {
+  return arr;
+};
+
+const arr = [1, 2, 3, 4, 5, 6];
+const x = 7;
+const result = shortestSubArray(arr, x);
+console.log(result);
